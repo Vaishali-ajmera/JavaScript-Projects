a580eb275b1f24392a4c8659eb64eb1c2997f8cd
Vaishali <vaishaliajmera11@gmail.com>
2022-10-07 13:35:10 +0530

    Adding more functionalities

--- Calculator App/index.js
+++ Calculator App/index.js
@@ -1,14 +1,99 @@
 const currentOutput = document.querySelector(".current-output");
 const previousOutput = document.querySelector(".previous-output");
+const temporaryResult = document.querySelector(".temporary-result");
 const operators = Array.from(document.querySelectorAll(".operator"));
 const numbers = Array.from(document.querySelectorAll(".number"));
 const equal = document.querySelector(".equal");
 
 let haveDot = false;
-let lastOperation = ""; //operator clicked
+let lastOperation = ""; 
 let previous = "";
 let current = "";
 let result = null;
+let tempResult = "";
+
+
+document.addEventListener("keydown",(e)=>{
+  // console.log(e.key);
+  buttonAnimation(e.key);
+
+})
+
+function buttonAnimation(currentKey){
+  // console.log(currentKey);
+  let key;
+   switch (currentKey) {
+    case '0':
+      key = "zero"
+      break;
+
+    case '1':
+      key = "one"
+      break;
+    case '2':
+      key = "two"
+      break;
+    case '3':
+      key = "three"
+      break;
+    case '4':
+      key = "four"
+      break;
+    case '5':
+      key = "five"
+      break;
+    case '6':
+      key = "six"
+      break;
+    case '7':
+      key = "seven"
+      break;
+    case '8':
+      key = "eight"
+      break;
+    case '9':
+      key = "nine"
+      break;
+    case '+':
+      key = "plus"
+      break;
+    case '-':
+      key = "minus"
+      break;
+    case '/':
+      key = "divide"
+      break;
+    case "*":
+      key = "multiply"
+      break;
+    case "Enter":
+      key = "equal"
+      break;
+   
+    case "Backspace":
+      key = "backspace"
+      break;
+
+    case ".":
+      key = "dot"
+      break;
+   
+    case "Delete":
+      key = "delete"
+      break;
+   
+    default:
+      console.log(currentKey)
+      break;
+   }
+   const button = document.querySelector("." + key);
+  //  console.log(button);
+   button.classList.add("pressed");
+   setTimeout(() => {
+     button.classList.remove("pressed");
+    
+   }, 100);
+}
 
 numbers.forEach((number) => {
   number.addEventListener("click", (e) => {
@@ -27,21 +112,32 @@ operators.map((operator) => {
     if (e.target.innerText === "C") {
       currentOutput.innerText = "";
       previousOutput.innerText = "";
+      temporaryResult.innerText = "";
       previous = "";
       current = "";
+      tempResult = "";
     } else if (e.target.innerText === "⌦") {
       currentOutput.innerText = currentOutput.innerText.slice(0, -1);
+      current = currentOutput.innerText;
     }
-    //normal operators
+    //normal operators + - * /
     else {
-      if (!current) {
+      if (!current && !previous) {
         return;
       }
 
       haveDot = false;
       const operationName = e.target.innerText;
-      if (current && previous && lastOperation) {
+      if(previous && lastOperation && !current){
+        // console.log(previous , current);
+        previousOutput.innerText = previousOutput.innerText.slice(0,-1);
+        previous = previousOutput.innerText;
+      }
+      else if (current && previous && lastOperation) {
+        // console.log(previous , current);
         mathOperation();
+        tempResult = result;
+        temporaryResult.innerText = tempResult;
       } else {
         result = parseFloat(current);
       }
@@ -79,8 +175,10 @@ equal.addEventListener("click", (e) => {
   mathOperation();
   clearVar();
   currentOutput.innerText = result;
+  temporaryResult.innerText = "";
   current = result;
   previous = "";
+  tempResult = "";
 });
 
 // to when a someone press button through keyboard
@@ -127,7 +225,7 @@ function clickButton(key) {
 }
 function clickOperation(key) {
   operators.forEach((operation) => {
-    console.log(operation.innerText)
+    // console.log(operation.innerText)
     if (operation.innerText === key) {
       operation.click();
     }
@@ -137,3 +235,4 @@ function clickOperation(key) {
 function clickEqual() {
   equal.click();
 }
+
